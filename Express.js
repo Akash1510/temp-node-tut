662b2a97e1990560701b6dc9909feb480ed96c82
Akash <aakashjadhav15102003@gmail.com>
2023-09-13 18:34:50 +0530

    nwe

--- Express.js
+++ Express.js
@@ -2,7 +2,7 @@
 const express  =require('express')
 const app = express()
 app .listen (7000, ()=>{
-    console.log('server is listening on port 5000 ...')
+    console.log('server is listening on port 5000 ..')
 })
 // app.get
 // app.post
